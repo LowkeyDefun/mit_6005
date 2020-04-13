bdc931ef8e49dba54be0a123938fc483c37ecee4
Jason Zhang <jasonzhang2013@outlook.com>
2020-04-13 15:31:44 +0100

    Reading 2

---
+++ hello_world/hello_world/src/hello_world/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello_world;
+
+
+/**
+ * 
+ * @author jason
+ * The HelloWorld class implements an application that
+ * simply prints "Hello World!" to standard output.
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); // Display the string.
+	}
+
+}
